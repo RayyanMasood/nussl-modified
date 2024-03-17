d40653c7ff128ab11d80e4d5f9bdf5ba07f96540
Ibn Masood <syed.rayyan.masood@gmail.com>
2024-03-17 12:12:22 +0500

    Update play_utils.py
    Changed 
    
    multitrack_template = pkg_resources.read_text(templates, 'multitrack.html') to
    
    multitrack_template = files(templates).joinpath('multitrack.html').read_text()
    
    To use the newer working code

--- nussl/core/play_utils.py
+++ nussl/core/play_utils.py
@@ -13,7 +13,11 @@ import importlib_resources as pkg_resources
 from . import templates
 from .utils import _close_temp_files
 
-multitrack_template = pkg_resources.read_text(templates, 'multitrack.html')
+from importlib_resources import files
+# Reads contents with UTF-8 encoding and returns str.
+multitrack_template = files(templates).joinpath('multitrack.html').read_text()
+
+# multitrack_template = pkg_resources.read_text(templates, 'multitrack.html')
 
 
 def _check_imports():
