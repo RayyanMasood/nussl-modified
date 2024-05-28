175f034c3a62d37036787d53e01c27bb179d5ff0
Ibn Masood <syed.rayyan.masood@gmail.com>
2024-05-28 16:38:07 +0500

    Testing changing array check from >3 dims to >4

--- nussl/core/masks/mask_base.py
+++ nussl/core/masks/mask_base.py
@@ -65,7 +65,7 @@ class MaskBase(object):
         if value.ndim == 2:
             value = np.expand_dims(value, axis=constants.STFT_CHAN_INDEX)
 
-        if value.ndim > 3:
+        if value.ndim > 4:
             raise ValueError('Cannot support arrays with more than 3 dimensions!')
 
         self._mask = self._validate_mask(value)
